2c80c69e9d7cac3d2f2f4606f4f72ea69b6bf31d
alinag-ditits <alinag@std.mans.edu.eg>
2026-03-25 15:36:57 +0200

    Update TDM.v

--- Project-9-Time-Division-Multiplexer/TDM.v
+++ Project-9-Time-Division-Multiplexer/TDM.v
@@ -1,1 +1,27 @@
-
+input [1:0] in0 , in1 , in2 , in3; 
+input clk , rst ; 
+output reg [1:0] out; 
+reg [1:0]counter ;  
+always @(posedge clk or posedge rst ) begin 
+if (rst) begin 
+    counter <= 2'b00 ;  
+end     
+else begin 
+    if(counter==2'b11) 
+    counter<=0 ;  
+    else begin 
+    counter <= counter + 1 ;  
+end  
+end 
+ 
+end 
+always @(*) begin 
+case (counter) 
+0 : out = in0 ;  
+1 : out = in1 ;  
+2 : out = in2 ;  
+3 : out = in3 ;  
+endcase     
+end 
+ 
+endmodule 
