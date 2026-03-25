2e613449e115bc2773d9acef51f7d38ed976bbcb
alinag-ditits <alinag@std.mans.edu.eg>
2026-03-25 14:19:45 +0200

    Update alu.v

--- Project-1-ALU/alu.v
+++ Project-1-ALU/alu.v
@@ -1,1 +1,14 @@
-
+module ALU #(parameter N = 4, parameter OPCODE = 0) (in0, in1, out);
+input [N-1:0] in0, in1;
+output reg [N-1:0] out;
+  
+always @(*) begin
+    case (OPCODE)
+        0: out = in0 + in1;
+        1: out = in0 | in1;
+        2: out = in0 - in1;
+        3: out = in0 ^ in1;
+    endcase
+end
+  
+endmodule
