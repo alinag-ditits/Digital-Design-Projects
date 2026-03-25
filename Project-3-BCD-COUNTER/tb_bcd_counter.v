9cbfe707e7776bef5518d8607115b988809e0e57
alinag-ditits <alinag@std.mans.edu.eg>
2026-03-25 14:51:17 +0200

    Update tb_bcd_counter.v

--- Project-3-BCD-COUNTER/tb_bcd_counter.v
+++ Project-3-BCD-COUNTER/tb_bcd_counter.v
@@ -1,1 +1,30 @@
+module tb_bcd();
 
+reg clk , rst;
+wire clk_div10_out;
+wire [3:0] count;
+
+BCD_COUNT DUT(clk , rst , clk_div10_out , count);
+
+initial begin
+    clk = 0;
+    forever begin
+        #1 clk = ~clk;
+    end
+end
+
+initial begin
+    rst = 1;
+    @(negedge clk);
+    rst = 0;
+
+    repeat(100) @(negedge clk);
+
+    $stop;
+end
+
+initial begin
+    $monitor("count = %b ||| clk_div = %b ||| rst = %b", count , clk_div10_out , rst);
+end
+
+endmodule
